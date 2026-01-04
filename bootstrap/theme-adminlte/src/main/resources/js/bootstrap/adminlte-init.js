7da44cdeccd4fff19ce7c0045a136797f760986e
Adrian Tarau <adrian.tarau@gmail.com>
2026-01-04 11:45:41 -0500

    Update AdminLTE theme

--- bootstrap/theme-adminlte/src/main/resources/js/bootstrap/adminlte-init.js
+++ bootstrap/theme-adminlte/src/main/resources/js/bootstrap/adminlte-init.js
@@ -1,25 +1,13 @@
-const SELECTOR_SIDEBAR_WRAPPER = ".sidebar-wrapper";
-const SELECTOR_MAIN_WRAPPER = ".app-main";
+const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
 const Default = {
-    scrollbarTheme: "os-theme-light",
-    scrollbarAutoHide: "leave",
+    scrollbarTheme: 'os-theme-light',
+    scrollbarAutoHide: 'leave',
     scrollbarClickScroll: true,
 };
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
     const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
-    const mainWrapper = false;//document.querySelector(SELECTOR_MAIN_WRAPPER);
-    const hasOverlayLib = typeof OverlayScrollbarsGlobal?.OverlayScrollbars !== "undefined";
-    if (sidebarWrapper && hasOverlayLib) {
+    if (sidebarWrapper && OverlayScrollbarsGlobal?.OverlayScrollbars !== undefined) {
         OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
-            scrollbars: {
-                theme: Default.scrollbarTheme,
-                autoHide: Default.scrollbarAutoHide,
-                clickScroll: Default.scrollbarClickScroll,
-            },
-        });
-    }
-    if (mainWrapper && hasOverlayLib) {
-        OverlayScrollbarsGlobal.OverlayScrollbars(mainWrapper, {
             scrollbars: {
                 theme: Default.scrollbarTheme,
                 autoHide: Default.scrollbarAutoHide,
